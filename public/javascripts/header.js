1fb3846096306b0633cef458b23b634ea7f98616
serpiko <serpiko@hanmail.net>
2025-04-11 00:13:52 +0900

    main css 작업중

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const normalHeader = document.querySelector(".header-normal");
-    const minHeader = document.querySelector(".header-min");
+  const normalHeader = document.querySelector(".header-normal");
+  const minHeader = document.querySelector(".header-min");
 
-    minHeader.style.display = "block";
-    const minHeaderHeight = minHeader.offsetHeight;
-    minHeader.style.display = "none";
+  minHeader.style.display = "flex";
+  const minHeaderHeight = minHeader.offsetHeight;
+  minHeader.style.display = "none";
 
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > minHeaderHeight) {
-        normalHeader.style.display = "none";
-        minHeader.style.display = "block";
-        } else {
-        normalHeader.style.display = "block";
-        minHeader.style.display = "none";
-        }
-    });
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > minHeaderHeight) {
+      normalHeader.style.display = "none";
+      minHeader.style.display = "flex";
+    } else {
+      normalHeader.style.display = "flex";
+      minHeader.style.display = "none";
+    }
+  });
 });
