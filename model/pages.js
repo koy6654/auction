a1feb4b4e45616c044ed64c04a6ae4725f066fea
serpiko <serpiko@hanmail.net>
2025-04-13 18:09:12 +0900

    component button complete

--- model/pages.js
+++ model/pages.js
@@ -110,11 +110,10 @@ export const details = [
   { route: "9_details", path: "9_2_email-form", title: "이메일 폼" },
 ];
 
-export const test = [
-  { route: "test", path: "test", title: "테스트페이지" },
-];
+export const test = [{ route: "test", path: "test", title: "테스트페이지" }];
 
 export const pages = [
+  ...test,
   ...template,
   ...main,
   ...auction,
